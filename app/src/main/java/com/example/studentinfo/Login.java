3e97e5394e54890d878a57db202e6bacab679fa5
davidoodugama1999@gmail.com <davidoodugama1999@gmail.com>
2020-08-14 18:26:49 +0530

    fist commit

---
+++ app/src/main/java/com/example/studentinfo/Login.java
@@ -0,0 +1,35 @@
+package com.example.studentinfo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+
+public class Login extends AppCompatActivity {
+
+    Handler handler;
+    Runnable runnable;
+    ImageView img;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        img = findViewById(R.id.img);
+        img.animate().alpha(4000).setDuration(0);
+
+        handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent dap = new Intent (Login.this,LoginPage.class);
+                startActivity(dap);
+                finish();
+            }
+        }, 4000);
+    }
+}
---
+++ app/src/main/java/com/example/studentinfo/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.studentinfo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.os.Bundle;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationview = findViewById(R.id.navigation_view);
+
+    }
+}
