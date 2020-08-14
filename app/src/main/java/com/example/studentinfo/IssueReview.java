d0df6f1344f41beeff7cec8aeb3861557bfbd700
davidoodugama1999@gmail.com <davidoodugama1999@gmail.com>
2020-08-14 22:55:23 +0530

    Issue Review completed

--- app/src/main/java/com/example/studentinfo/IssueReview.java
+++ app/src/main/java/com/example/studentinfo/IssueReview.java
@@ -3,12 +3,30 @@ package com.example.studentinfo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.EditText;
 
 public class IssueReview extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_issue_review);
+
+        EditText textArea = (EditText) findViewById(R.id.textArea_info);
+        textArea.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                v.getParent().requestDisallowInterceptTouchEvent(true);
+                switch(event.getAction() & MotionEvent.ACTION_MASK){
+                    case MotionEvent.ACTION_UP:
+                        v.getParent().requestDisallowInterceptTouchEvent(false);
+                        break;
+                }
+                return false;
+            }
+        });
     }
 }
