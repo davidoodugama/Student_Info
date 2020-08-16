810a313c725aa21119cf7375a1b5f446926641da
MiyuruWijesinghe <mkwijesinghe2016@gmail.com>
2020-08-16 13:04:19 +0530

    miyuru added pages

---
+++ app/src/main/java/com/example/studentinfo/NewStudentsList.java
@@ -0,0 +1,14 @@
+package com.example.studentinfo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewStudentsList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_students_list);
+    }
+}
